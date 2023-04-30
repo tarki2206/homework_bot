3fd74f6face08ee0fd846eb5509e019dc060b975
tarki2206 <sasha100500@yahoo.com>
2023-04-30 09:55:11 +0200

    22

--- homework.py
+++ homework.py
@@ -1,16 +1,28 @@
-...
+from dotenv import load_dotenv
+from http import HTTPStatus
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+import telegram
+from telegram import Bot
+import time
+import requests
+import json
+
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('token_practicum')
+TELEGRAM_TOKEN = os.getenv('bot_token')
+TELEGRAM_CHAT_ID = os.getenv('chat_id')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
+timestamp = int(time.time())
+payload = {'from_date': timestamp}
 
 HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
@@ -18,48 +30,126 @@ HOMEWORK_VERDICTS = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='main.log',
+    filemode='w',
+    format='%(asctime)s, %(levelname)s, %(message)s'
+)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = RotatingFileHandler('main.log', maxBytes=50000000, backupCount=5)
+logger.addHandler(handler)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+handler.setFormatter(formatter)
+
 
 def check_tokens():
-    ...
+    """Function for checking tokens."""
+    check_tokens_fun = all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
+    if check_tokens_fun:
+        return True
+    if not check_tokens_fun:
+        message_error = 'No, tokens'
+        logger.critical(message_error)
+        return False
 
 
 def send_message(bot, message):
-    ...
+    """Function for sending message."""
+    try:
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+        logger.debug(f'Message sent: {message}')
+        raise Exception('Message sent')
+
+    except telegram.TelegramError:
+        message_error = f"Message wasn't sent: {message}"
+        logger.error(message_error)
+    except Exception as e:
+        logger.error(f'{e}')
 
 
 def get_api_answer(timestamp):
-    ...
+    """Function for getting api answer."""
+    params = {'from_date': timestamp}
+    try:
+        homework = requests.get(url=ENDPOINT, headers=HEADERS, params=params)
+        status_code = homework.status_code
+        if status_code != HTTPStatus.OK:
+            message_error = 'Wrong response'
+            raise Exception(message_error)
+        return homework.json()
+    except requests.exceptions.RequestException:
+        message_error = 'Request Exception'
+        raise Exception(message_error)
+    except json.JSONDecodeError:
+        message_error = 'Problem with json'
+        raise Exception(message_error)
 
 
 def check_response(response):
-    ...
+    """Function for checking response."""
+    if type(response) != dict:
+        raise TypeError('Wrong type')
+    homework_statuses = response.get('homeworks')
+    if (type(homework_statuses) != list) or\
+            (type(homework_statuses[0]) != dict):
+        raise TypeError('Wrong type')
+    else:
+        return []
 
 
 def parse_status(homework):
-    ...
+    """Function for parsing status."""
+    try:
+        homework_name = homework.get('homework_name')
+        homework_status = homework.get('status')
+        result = HOMEWORK_VERDICTS[homework_status]
+        if homework_status not in HOMEWORK_VERDICTS:
+            raise KeyError(f'Invalid homework status: {homework_status}')
+        if homework_name is None:
+            raise Exception('Wrong response')
+        return f'Изменился статус проверки работы "{homework_name}". {result}'
 
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    except KeyError as error:
+        logging.error(f'Error {error}')
+        raise Exception(KeyError)
 
 
 def main():
-    """Основная логика работы бота."""
+    """Main function."""
+    if not check_tokens():
+        exit()
+    else:
+        previous_status = []
+        while True:
+            try:
+                response = get_api_answer(timestamp)
+                check_response(response)
+                if response:
+                    current_status = response['homeworks']
+                    print(current_status)
+                    if current_status != previous_status:
+                        bot = Bot(token=TELEGRAM_TOKEN)
+                        homework = current_status[0]
+                        text = parse_status(homework)
+                        send_message(bot, text)
+                        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=text)
+                        logger.debug('Message was sent second time')
+                    previous_status = current_status
 
-    ...
+            except Exception as error:
+                logging.error(f'{error}')
 
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    timestamp = int(time.time())
+            except telegram.TelegramError as e:
+                message_error = f"Message wasn't sent {e}"
+                logger.error(message_error)
 
-    ...
-
-    while True:
-        try:
-
-            ...
-
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
-        ...
+            finally:
+                time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
---
+++ testing_bot.py
@@ -0,0 +1,9 @@
+"""from telegram import Bot
+
+def bot_mes():
+    text = "wwww"
+    bot = Bot(token='6062936462:AAH5o9p7WRnFAhGxarYY9qC6xepoVGP1ub8')
+    print('start')
+    bot.send_message(chat_id=902751572, text=text)
+
+bot_mes()"""
