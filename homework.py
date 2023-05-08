33fd491b3c809eb4ebbaebb0c08aa2874c429c14
tarki2206 <sasha100500@yahoo.com>
2023-05-08 14:38:22 +0200

    32

--- homework.py
+++ homework.py
@@ -84,29 +84,27 @@ def check_response(response):
         raise TypeError(info_message)
     homeworks = response.get('homeworks')
     if not isinstance(homeworks, list):
-        second_info_message = f'Was expected list type, {type(homeworks)}'
-        logger.error(second_info_message)
-        raise TypeError(second_info_message)
+        info_message = f'Was expected list type, {type(homeworks)}'
+        logger.error(info_message)
+        raise TypeError(info_message)
 
 
 def parse_status(homework):
     """Function for parsing status."""
     keys = {'name': homework.get('homework_name'),
             'status': homework.get('status')}
-    homework_name = homework.get('homework_name')
-    homework_status = homework.get('status')
     for name, value in keys.items():
         if not value:
             info_message = f'No key {name}'
             logger.error(info_message)
             raise KeyError(info_message)
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
     if homework_status not in HOMEWORK_VERDICTS:
         raise KeyError('Undefined status homework')
     if not homework_name:
         raise KeyError('Name field is empty')
-    if homework_name and homework_status:
-        logger.info('Keys was getting')
-        result = HOMEWORK_VERDICTS[homework_status]
+    result = HOMEWORK_VERDICTS[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {result}'
 
 
@@ -119,9 +117,9 @@ def main():
             response = get_api_answer(timestamp)
             check_response(response)
             homeworks_list = response['homeworks']
-            homework, *_ = homeworks_list
-            status_homework = parse_status(homework)
             if homeworks_list:
+                homework, *_ = homeworks_list
+                status_homework = parse_status(homework)
                 if status_homework != previous_status:
                     bot = Bot(token=TELEGRAM_TOKEN)
                     send_message(bot, status_homework)
